731b2ba53c97c59234b8f4c401a90026f2d3aed5
kroeberd <59828746+kroeberd@users.noreply.github.com>
2024-09-02 16:36:10 +0200

    Update lib.py

--- user.script.version/lib.py
+++ user.script.version/lib.py
@@ -6,20 +6,19 @@ import time
 
 # by Denny Kroeber / Sarcasm / kroeberd
 
+if len(sys.argv) != 6:
+    print("Usage: python lib.py <PLEX_TOKEN> <folder_path> <PLEX_AGENT> <PLEX_SCANNER> <PLEX_URL>")
+    sys.exit(1)
+
 PLEX_TOKEN = sys.argv[1]
 folder_path = sys.argv[2]
 PLEX_AGENT = sys.argv[3]
 PLEX_SCANNER = sys.argv[4]
 PLEX_URL = sys.argv[5]
+
+print(f"base_folder: {folder_path}")
+
 plex = PlexServer(PLEX_URL, PLEX_TOKEN)
-
-# debug print
-print(f"PLEX_URL: {PLEX_URL}")
-print(f"PLEX_TOKEN: {PLEX_TOKEN}")
-print(f"PLEX_AGENT: {PLEX_AGENT}")
-print(f"PLEX_SCANNER: {PLEX_SCANNER}")
-print(f"folder_path: {folder_path}")
-
 
 def create_library(library_name, folder_path, agent, scanner):
     print(f'Erstelle Mediathek: {library_name} für den Pfad: {folder_path}')
@@ -47,8 +46,8 @@ def create_library(library_name, folder_path, agent, scanner):
 def main():
     sections = plex.library.sections()
     existing_libraries = {section.title for section in sections}
-    for folder_name in os.listdir(base_folder):
-        folder_path = os.path.join(base_folder, folder_name)
+    for folder_name in os.listdir(folder_path):
+        folder_path = os.path.join(folder_path, folder_name)
         if os.path.isdir(folder_path):  # Sicherstellen, dass es ein Ordner ist
             if folder_name not in existing_libraries:
                 create_library(folder_name, folder_path, PLEX_AGENT, PLEX_SCANNER)
