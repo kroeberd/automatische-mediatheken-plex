57aaa918927f88480661ca3c4bbcaa6229172f00
kroeberd <59828746+kroeberd@users.noreply.github.com>
2024-09-02 15:19:51 +0200

    alpha

--- user.script.version/lib.py
+++ user.script.version/lib.py
@@ -7,8 +7,62 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 # Read the Plex token from the command-line arguments
-plex_token = sys.argv[1]
+PLEX_TOKEN = sys.argv[1]
+
+# UNC-Path
+base_folder = r'/media'  # Hier den Pfad zum Stammverzeichnis
+
+# Plex Agent and Scanner
+PLEX_AGENT = 'com.plexapp.agents.stashplexagent'  # Standard Plex Movie Agent
+PLEX_SCANNER = 'Plex Movie Scanner'  # Standard Plex Movie Scanner
 
 # Your existing Plex setup and logic
-baseurl = 'http://127.0.0.1:32400'  # Replace with your Plex server URL
-plex = PlexServer(baseurl, plex_token)
+PLEX_URL = 'http://127.0.0.1:32400'  # Replace with your Plex server URL
+plex = PlexServer(PLEX_URL, PLEX_TOKEN)
+
+def create_library(library_name, folder_path, agent, scanner):
+
+    print(f'Erstelle Mediathek: {library_name} für den Pfad: {folder_path}')
+
+    # Anfrage-URL
+    url = f'{PLEX_URL}/library/sections'
+
+    # Parameter für die Anfrage
+    params = {
+        'name': library_name,
+        'type': 'movie',
+        'agent': agent,
+        'scanner': scanner,
+        'language': 'en',
+        'location': folder_path
+    }
+  
+    headers = {
+        'X-Plex-Token': PLEX_TOKEN
+    }
+
+    # Sende die Anfrage an den Plex-Server
+    response = requests.post(url, headers=headers, params=params)
+
+    if response.status_code == 201:
+        print(f'Mediathek {library_name} erfolgreich erstellt.')
+    else:
+        print(f"Fehler beim Erstellen der Mediathek {library_name}: {response.status_code} {response.text}")
+
+def main():
+    sections = plex.library.sections()
+    existing_libraries = {section.title for section in sections}
+
+    # Durchlaufe alle Unterordner im Basisordner
+    for folder_name in os.listdir(base_folder):
+        folder_path = os.path.join(base_folder, folder_name)
+
+        if os.path.isdir(folder_path):  # Sicherstellen, dass es ein Ordner ist
+            if folder_name not in existing_libraries:
+                create_library(folder_name, folder_path, PLEX_AGENT, PLEX_SCANNER)
+                time.sleep(5)  # 5 Sekunden warten, um zu vermeiden, dass die API überlastet wird
+            else:
+                print(f"Mediathek {folder_name} existiert bereits.")
+
+if __name__ == "__main__":
+    main()
